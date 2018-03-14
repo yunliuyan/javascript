4a19238a6ae94a4acd57fa0e715edd21ae37717e
yunliuyan <1038733742@qq.com>
2018-03-14 16:06:05 +0800

    雪花背景画廊

---
+++ snow/js/myData.js
@@ -0,0 +1,108 @@
+/**
+ * Created by Administrator on 2015/4/25.
+ */
+    //定义mydata数组 , 用来分段返回值.
+    var myData = [
+        {
+            img: "1.jpg",
+            caption: "仲春之旅，与你同游",
+            desc: "洪崖洞啊，闪闪发亮，煞是好看，不过静儿最好看啦。有桥，有水，有光，有你。"
+
+        },
+        {
+            img: "2.jpg",
+            caption: "仲春之旅",
+            desc: "这也是洪崖洞的照片呀"
+
+        },
+        {
+            img: "3.jpg",
+            caption: "仲春之旅",
+            desc: "下次去的话就跟你去下游坐船，便宜哈哈哈~"
+
+        },
+        {
+            img: "4.jpg",
+            caption: "仲春之旅",
+            desc: "静儿的拍照好看呀"
+
+        },
+        {
+            img: "5.jpg",
+            caption: "仲春之旅",
+            desc: "好像没去过这里额，不过这个角度看，重庆真美"
+
+        },
+        {
+            img: "6.jpg",
+            caption: "仲春之旅",
+            desc: "lpl比赛呀，马哥真的惨，被群殴了好几次了，心疼心疼"
+
+        },
+        {
+            img: "7.jpg",
+            caption: "仲春之旅",
+            desc: "下次的话，一定要睡的饱饱的，再去看比赛。王多多的声音太催眠啦"
+
+        },
+        {
+            img: "8.jpg",
+            caption: "仲春之旅",
+            desc: "这是静儿给我画的，好喜欢"
+
+        },
+        {
+            img: "9.jpg",
+            caption: "仲春之旅",
+            desc: "这也是静儿给我的，身无彩凤双飞翼，心有灵犀一点通"
+
+        },    {
+            img: "10.jpg",
+            caption: "仲春之旅",
+            desc: "嘻嘻嘻，这是我从你的微信头像扒下来的"
+
+        },
+        {
+            img: "11.jpg",
+            caption: "仲春之旅",
+            desc: "我们的唯一一张合拍，珍藏珍藏"
+
+        },
+        {
+            img: "12.jpg",
+            caption: "仲春之旅",
+            desc: "大写的洪崖洞"
+
+        },
+        {
+            img: "13.jpg",
+            caption: "仲春之旅",
+            desc: "似乎洪崖洞的照片有点多呀"
+
+        },
+        {
+            img: "14.jpg",
+            caption: "仲春之旅",
+            desc: "静儿的拍照技术好棒"
+
+        },
+        {
+            img: "17.jpg",
+            caption: "仲春之旅",
+            desc: "真的好喜欢好喜欢我家静儿呀"
+
+        },
+        {
+            img: "18.jpg",
+            caption: "仲春之旅",
+            desc: "以后跟你看晨曦啊"
+
+        },
+        {
+            img: "20.jpg",
+            caption: "仲春之旅",
+            desc: "snake vs top"
+
+        }
+    ] ;
+    
